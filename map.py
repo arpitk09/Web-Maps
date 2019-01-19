2430658b5d6f4da97054bc972d97bd03eb45a9dc
Arpit Kashyap <arpit@Arpits-MacBook-Air.local>
2019-01-19 16:06:25 +0530

    `t message for your changes. Lines starting

--- map.py
+++ map.py
@@ -8,7 +8,9 @@
 import folium
 
 #Create base map
-map = folium.Map(location=[28.5799217,77.0546595], zoom_start = 8)
+map = folium.Map(location=[28.5799217,77.0546595], zoom_start = 8, tiles="Mapbox bright")
+
+folium.Marker(location=[28.5799217,77.0546595],popup="Punjab National National Bank", icon=folium.Icon(color='gray')).add_to(map)
 
 #Save the map
 map.save("map1.html")
