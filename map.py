e386ca68b1716b0fe75177b5ee8fbcbe6a71a44d
Arpit Kashyap <arpit@Arpits-MacBook-Air.local>
2019-01-19 14:45:16 +0530

    New
    	modified:   map.py

---
+++ map.py
@@ -0,0 +1,14 @@
+# import folium
+
+# map = folium.Map(location=[28.5799217,77.0546595], zoom_start=8)
+
+# map.save("map1.html")
+
+
+import folium
+
+#Create base map
+map = folium.Map(location=[28.5799217,77.0546595], zoom_start = 8)
+
+#Save the map
+map.save("map1.html")
