0915fe2544126a43f09cac60dca31d8fc2862d80
Arpit Kashyap <arpit@Arpits-MacBook-Air.local>
2019-01-21 13:43:47 +0530

    Color Chnage

--- map.py
+++ map.py
@@ -11,7 +11,7 @@ map = folium.Map(location=[37.296933,-121.9574983], zoom_start=8, tiles="Mapbox 
 
 # ? Multi markers handelling 
 for coordinates in [[37.4074687,-122.086669],[37.8199286,-122.4804438]]:
-    folium.Marker(location=coordinates, icon=folium.Icon(color="green")).add_to(map)
+    folium.Marker(location=coordinates, icon=folium.Icon(color="red")).add_to(map)
 
 # ? Save the map
 map.save("map1.html")
